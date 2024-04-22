19027cd9c5a19d5d5e9724cc0c70edbbebfa71da
alhasann351 <alhasann352@gmail.com>
2024-04-23 01:56:43 +0600

    data-show

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'models/posts_model.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,6 +11,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String id = 'ID : ';
+  String title = 'Title : ';
+  String body = 'Body : ';
+  List<PostsModel> postsModelList = [];
+
+  Future<List<PostsModel>> getPostsApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+
+    if (response.statusCode == 200) {
+      for (Map<dynamic, dynamic> posts in data) {
+        postsModelList.add(PostsModel.fromJson(posts));
+      }
+      return postsModelList;
+    } else {
+      return postsModelList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,11 +46,44 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: const Column(
-        children: [
-          Text('Rest Api')
-        ],
-      ),
+      body: Column(children: [
+        Expanded(
+          child: FutureBuilder(
+            future: getPostsApi(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: Text(
+                    'Loading',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                );
+              } else {
+                return ListView.builder(itemCount: postsModelList.length, itemBuilder: (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('$id ${postsModelList[index].id}'),
+                          Text('$title ${postsModelList[index].title.toString()}'),
+                          Text('$body ${postsModelList[index].body.toString()}'),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+              }
+            },
+          ),
+        ),
+      ]),
     );
   }
 }
