d5e9e9d70abaecb26ad19f0a88d08c68305df9ee
alhasann351 <alhasann352@gmail.com>
2024-05-01 04:44:08 +0600

    show-user-data-without-model-created

--- lib/without_model_create.dart
+++ lib/without_model_create.dart
@@ -14,12 +14,13 @@ class _WithoutModelCreate extends State<WithoutModelCreate> {
 
   var data;
 
-  Future<void> getApi() async{
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+  Future<void> getApi() async {
+    final response = await http.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       data = jsonDecode(response.body.toString());
-    }else{
+    } else {
       print('Error');
     }
   }
@@ -28,18 +29,114 @@ class _WithoutModelCreate extends State<WithoutModelCreate> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        title: const Text(
-          'Without Model Create Rest Api',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          title: const Text(
+            'Without Model Create Rest Api',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
+        body: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                  future: getApi(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: Text(
+                          'Loading',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Card(
+                                elevation: 10,
+                                color: Colors.white,
+                                shadowColor: Colors.grey,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment
+                                        .start,
+                                    children: [
+                                      ReusableRow(title: 'Name: ',
+                                          value: data![index]['name']
+                                              .toString()),
+                                      ReusableRow(title: 'Use Name: ',
+                                          value: data![index]['username']
+                                              .toString()),
+                                      ReusableRow(title: 'Email: ',
+                                          value: data![index]['email']
+                                              .toString()),
+                                      const Padding(
+                                        padding: EdgeInsets.only(left: 8.0),
+                                        child: Text(
+                                          'Address: ', style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),),
+                                      ),
+                                      ReusableRow(
+                                          title: '               Street: ',
+                                          value: data![index]['address']!['street']
+                                              .toString()),
+                                      ReusableRow(
+                                          title: '               suite: ',
+                                          value: data![index]['address']!['suite']
+                                              .toString()),
+                                      ReusableRow(
+                                          title: '               City: ',
+                                          value: data![index]['address']!['city']
+                                              .toString()),
+                                      ReusableRow(
+                                          title: '               Zipcode: ',
+                                          value: data![index]['address']!['zipcode']
+                                              .toString()),
+                                      const Padding(
+                                        padding: EdgeInsets.only(left: 8.0),
+                                        child: Text('               Geo: ',
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black,
+                                          ),),
+                                      ),
+                                      ReusableRow(
+                                          title: '                       Lat: ',
+                                          value: data![index]['address']!['geo']!['lat']
+                                              .toString()),
+                                      ReusableRow(
+                                          title: '                       Lng: ',
+                                          value: data![index]['address']!['geo']!['lng']
+                                              .toString()),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          });
+                    }
+                  }),
+            ),
+          ],
+        ),
     );
   }
 }
