206de9ed2c07cd69cc747a00aa18512198bd05e3
alhasann351 <alhasann352@gmail.com>
2024-05-01 02:29:53 +0600

    get-api-data

--- lib/api_integration.dart
+++ lib/api_integration.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'models/photos_model.dart';
 
 class ApiIntegration extends StatefulWidget {
   const ApiIntegration({super.key});
@@ -9,6 +12,25 @@ class ApiIntegration extends StatefulWidget {
 }
 
 class _ApiIntegration extends State<ApiIntegration> {
+  
+  List<Photos> photosModelList = [];
+  
+  Future<List<Photos>> getPhotosApi() async{
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+    var data = jsonDecode(response.body.toString());
+    
+    if(response.statusCode == 200){
+      photosModelList.clear();
+      for(Map<dynamic, dynamic> photo in data){
+        Photos photos = Photos(title: photo['title'], url: photo['url'], id: photo['id']);
+        photosModelList.add(photos);
+      }
+      return photosModelList;
+    }else{
+      return photosModelList;
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
