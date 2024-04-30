6116bb842570e7ba0db1ce1fecc4103bc8660eb2
alhasann351 <alhasann352@gmail.com>
2024-05-01 02:58:54 +0600

    show-api-data

--- lib/api_integration.dart
+++ lib/api_integration.dart
@@ -12,25 +12,29 @@ class ApiIntegration extends StatefulWidget {
 }
 
 class _ApiIntegration extends State<ApiIntegration> {
-  
+  String id = 'ID: ';
+  String title = 'Title: ';
+
   List<Photos> photosModelList = [];
-  
-  Future<List<Photos>> getPhotosApi() async{
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+
+  Future<List<Photos>> getPhotosApi() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
     var data = jsonDecode(response.body.toString());
-    
-    if(response.statusCode == 200){
+
+    if (response.statusCode == 200) {
       photosModelList.clear();
-      for(Map<dynamic, dynamic> photo in data){
-        Photos photos = Photos(title: photo['title'], url: photo['url'], id: photo['id']);
+      for (Map<dynamic, dynamic> photo in data) {
+        Photos photos =
+            Photos(title: photo['title'], url: photo['url'], id: photo['id']);
         photosModelList.add(photos);
       }
       return photosModelList;
-    }else{
+    } else {
       return photosModelList;
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,6 +49,65 @@ class _ApiIntegration extends State<ApiIntegration> {
             color: Colors.white,
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPhotosApi(),
+              builder: (context, AsyncSnapshot<List<Photos>> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: Text(
+                      'Loading...',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: photosModelList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Card(
+                          elevation: 10,
+                          shadowColor: Colors.grey,
+                          color: Colors.white,
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  snapshot.data![index].url.toString()),
+                            ),
+                            title: Text(
+                              '$id ${snapshot.data![index].id.toString()}',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
+                            ),
+                            subtitle: Text(
+                              '$title ${snapshot.data![index].title.toString()}',
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
