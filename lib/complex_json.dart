75fc62a3f9e8a839f191472ef2e54c060405d1c8
alhasann351 <alhasann352@gmail.com>
2024-05-03 22:00:50 +0600

    api-get

--- lib/complex_json.dart
+++ lib/complex_json.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+import 'models/complex_json_model.dart';
 
 class ComplexJson extends StatefulWidget {
   const ComplexJson({super.key});
@@ -9,6 +13,18 @@ class ComplexJson extends StatefulWidget {
 }
 
 class _ComplexJsonState extends State<ComplexJson> {
+
+  Future<ComplexJsonModel> getApi() async{
+    final response = await http.get(Uri.parse('https://webhook.site/a2bb12bb-ac0d-493b-9368-b42520370925'));
+    var data = jsonDecode(response.body.toString());
+
+    if(response.statusCode == 200){
+      return ComplexJsonModel.fromJson(data);
+    }else{
+      return ComplexJsonModel.fromJson(data);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
