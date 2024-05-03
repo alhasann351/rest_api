b0110ab6fd4e409a4bcefb2cb27c344d05acbc4d
alhasann351 <alhasann352@gmail.com>
2024-05-03 22:58:35 +0600

    show-data

--- lib/complex_json.dart
+++ lib/complex_json.dart
@@ -13,14 +13,14 @@ class ComplexJson extends StatefulWidget {
 }
 
 class _ComplexJsonState extends State<ComplexJson> {
-
-  Future<ComplexJsonModel> getApi() async{
-    final response = await http.get(Uri.parse('https://webhook.site/a2bb12bb-ac0d-493b-9368-b42520370925'));
+  Future<ComplexJsonModel> getApi() async {
+    final response = await http.get(
+        Uri.parse('https://webhook.site/a2bb12bb-ac0d-493b-9368-b42520370925'));
     var data = jsonDecode(response.body.toString());
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       return ComplexJsonModel.fromJson(data);
-    }else{
+    } else {
       return ComplexJsonModel.fromJson(data);
     }
   }
@@ -39,6 +39,62 @@ class _ComplexJsonState extends State<ComplexJson> {
             color: Colors.white,
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: FutureBuilder<ComplexJsonModel>(
+            future: getApi(),
+            builder: (context, snapshot) {
+              if(snapshot.hasData){
+                return ListView.builder(
+                    itemCount: snapshot.data!.data!.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.3,
+                            width: MediaQuery.of(context).size.width * 1,
+                            color: Colors.red,
+                            child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: snapshot.data!.data![index].images!.length,
+                                itemBuilder: (context, position) {
+                                  return Container(
+                                    //color: Colors.green,
+                                    height:
+                                    MediaQuery.of(context).size.height * 0.25,
+                                    width:
+                                    MediaQuery.of(context).size.width * 0.5,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Image.network(snapshot
+                                          .data!.data![index].images![position].url
+                                          .toString()),
+                                    ),
+                                  );
+                                }),
+                          ),
+                        ],
+                      );
+                    });
+              }else{
+                return const Center(
+                  child: Text(
+                    'Loading',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                );
+              }
+            },
+          )),
+        ],
       ),
     );
   }
